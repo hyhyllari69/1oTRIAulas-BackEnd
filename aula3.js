6c68aed776c88f4c9ddcf8ee0e5ed59c9e66ba28
Hyllari <isadora.barbosa29@escola.pr.gov.br>
2026-02-25 07:58:56 -0300

    Add files via upload

---
+++ aula3.js
@@ -0,0 +1,21 @@
+// BOOLEANOS: "true" e " false"
+/*Operadores de comparação:
+== -> Igualdade
+!= -> Desigualdade 
+> -> Maior que
+< -> Menor que
+>= -> Maior ou igual que
+<= -> Menor ou igual que
+*Operador de igualdade estrita*
+=== -> Igualdade estrita
+!== -> Desigualdade estrita
+*/
+console.log(10 == "10")
+console.log(true == 1)
+console.log(false == 0)
+
+// Igualdade Estrita
+
+console.log(10 === 1)
+console.log(true === 1)
+console.log(false === 0)
